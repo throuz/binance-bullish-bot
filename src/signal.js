4e6ac86a6af6fc921aaa5afb12daa8377aca4fe4
HSU JUI SHENG <zed2786328@gmail.com>
2023-04-13 12:16:25 +0800

    optimize signal file

--- src/signal.js
+++ src/signal.js
@@ -5,7 +5,7 @@ import { taAPI } from "./web-services.js";
 const { TAAPI_SECRET } = env;
 const { BASE_ASSET, QUOTE_ASSET } = tradeConfig;
 
-const getSignal = async () => {
+const getSignals = async () => {
   const response = await taAPI.post("/bulk", {
     secret: TAAPI_SECRET,
     construct: {
@@ -29,118 +29,115 @@ const getSignal = async () => {
   });
 
   const signals = response.data.data.map(({ id, result }) => {
-    if (id === "cci") {
-      if (result.value > 100) {
-        return "BUY";
-      }
-      if (result.value < -100) {
-        return "SELL";
-      }
-    }
+    switch (id) {
+      case "cci":
+        if (result.value > 100) {
+          return "BUY";
+        }
+        if (result.value < -100) {
+          return "SELL";
+        }
 
-    if (id === "cmf") {
-      if (result.value > 0) {
-        return "BUY";
-      }
-      if (result.value < 0) {
-        return "SELL";
-      }
-    }
+      case "cmf":
+        if (result.value > 0) {
+          return "BUY";
+        }
+        if (result.value < 0) {
+          return "SELL";
+        }
 
-    if (id === "fibonacciretracement") {
-      if (result.trend === "UPTREND") {
-        return "BUY";
-      }
-      if (result.trend === "DOWNTREND") {
-        return "SELL";
-      }
-    }
+      case "fibonacciretracement":
+        if (result.trend === "UPTREND") {
+          return "BUY";
+        }
+        if (result.trend === "DOWNTREND") {
+          return "SELL";
+        }
 
-    if (id === "mfi") {
-      if (result.value < 20) {
-        return "BUY";
-      }
-      if (result.value > 80) {
-        return "SELL";
-      }
-    }
+      case "mfi":
+        if (result.value < 20) {
+          return "BUY";
+        }
+        if (result.value > 80) {
+          return "SELL";
+        }
 
-    if (id === "mom") {
-      if (result.value > 0) {
-        return "BUY";
-      }
-      if (result.value < 0) {
-        return "SELL";
-      }
-    }
+      case "mom":
+        if (result.value > 0) {
+          return "BUY";
+        }
+        if (result.value < 0) {
+          return "SELL";
+        }
 
-    if (id === "rsi") {
-      if (result.value < 30) {
-        return "BUY";
-      }
-      if (result.value > 70) {
-        return "SELL";
-      }
-    }
+      case "rsi":
+        if (result.value < 30) {
+          return "BUY";
+        }
+        if (result.value > 70) {
+          return "SELL";
+        }
 
-    if (id === "stoch") {
-      if (result.valueK < 20 && result.valueD < 20) {
-        return "BUY";
-      }
-      if (result.valueK > 80 && result.valueD > 80) {
-        return "SELL";
-      }
-    }
+      case "stoch":
+        if (result.valueK < 20 && result.valueD < 20) {
+          return "BUY";
+        }
+        if (result.valueK > 80 && result.valueD > 80) {
+          return "SELL";
+        }
 
-    if (id === "stochrsi") {
-      if (result.valueK < 20 && result.valueD < 20) {
-        return "BUY";
-      }
-      if (result.valueK > 80 && result.valueD > 80) {
-        return "SELL";
-      }
-    }
+      case "stochrsi":
+        if (result.valueK < 20 && result.valueD < 20) {
+          return "BUY";
+        }
+        if (result.valueK > 80 && result.valueD > 80) {
+          return "SELL";
+        }
 
-    if (id === "supertrend") {
-      if (result.valueAdvice === "long") {
-        return "BUY";
-      }
-      if (result.valueAdvice === "short") {
-        return "SELL";
-      }
-    }
+      case "supertrend":
+        if (result.valueAdvice === "long") {
+          return "BUY";
+        }
+        if (result.valueAdvice === "short") {
+          return "SELL";
+        }
 
-    if (id === "trix") {
-      if (result.value > 0) {
-        return "BUY";
-      }
-      if (result.value < 0) {
-        return "SELL";
-      }
-    }
+      case "trix":
+        if (result.value > 0) {
+          return "BUY";
+        }
+        if (result.value < 0) {
+          return "SELL";
+        }
 
-    if (id === "ultosc") {
-      if (result.value < 30) {
-        return "BUY";
-      }
-      if (result.value > 70) {
-        return "SELL";
-      }
-    }
+      case "ultosc":
+        if (result.value < 30) {
+          return "BUY";
+        }
+        if (result.value > 70) {
+          return "SELL";
+        }
 
-    return "NONE";
+      default:
+        return "NONE";
+    }
   });
 
+  return signals;
+};
+
+const getSignal = async () => {
+  const signals = await getSignals();
   const buyCount = signals.filter((signal) => signal === "BUY").length;
   const sellCount = signals.filter((signal) => signal === "SELL").length;
-
-  if (buyCount >= 6) {
+  const halfLengthOfSignals = Math.floor(signals.length / 2);
+  if (buyCount > halfLengthOfSignals) {
     return "BUY";
   }
-  if (sellCount >= 6) {
+  if (sellCount > halfLengthOfSignals) {
     return "SELL";
   }
   return "NONE";
 };
 
-export { getSignal };
+export { getSignals, getSignal };
