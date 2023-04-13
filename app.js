8d38edeaf61a74fdee99995801db67219049ceb0
HSU JUI SHENG <zed2786328@gmail.com>
2023-04-13 11:23:02 +0800

    abstract error handler

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { sendLineNotify } from "./src/common.js";
+import { errorHandler } from "./src/common.js";
 import getSignal from "./src/getSignal.js";
 import { openPosition, closePosition } from "./src/trade.js";
 
@@ -10,23 +10,7 @@ const check = async () => {
       await openPosition(signal);
     }
   } catch (error) {
-    if (error.response) {
-      await sendLineNotify(
-        "The request was made and the server responded with a status code that falls out of the range of 2xx"
-      );
-      console.log(error.response.data);
-      console.log(error.response.status);
-      console.log(error.response.headers);
-    } else if (error.request) {
-      await sendLineNotify("The request was made but no response was received");
-      console.log(error.request);
-    } else {
-      await sendLineNotify(
-        "Something happened in setting up the request that triggered an Error"
-      );
-      console.log("Error", error.message);
-    }
-    console.log(error.config);
+    await errorHandler(error);
   }
 };
 
--- src/common.js
+++ src/common.js
@@ -8,4 +8,20 @@ const logWithTime = (msg) => {
   console.log(`${msg} [${new Date().toLocaleString()}]`);
 };
 
-export { sendLineNotify, logWithTime };
+const errorHandler = async (error) => {
+  if (error.response) {
+    await sendLineNotify("Response status code is outside the 2xx range");
+    console.log(error.response.data);
+    console.log(error.response.status);
+    console.log(error.response.headers);
+  } else if (error.request) {
+    await sendLineNotify("No response was received");
+    console.log(error.request);
+  } else {
+    await sendLineNotify("Error occurred during request setup.");
+    console.log("Error", error.message);
+  }
+  console.log(error.config);
+};
+
+export { sendLineNotify, logWithTime, errorHandler };
