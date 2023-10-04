3e70b457cfc96eb134c0f64c33a79f390c408b52
Throuz <zed2786328@gmail.com>
2023-10-04 06:00:49 +0000

    api cache

--- src/api.js
+++ src/api.js
@@ -2,8 +2,9 @@ import crypto from "node:crypto";
 import querystring from "node:querystring";
 import { binanceFuturesAPI } from "./web-services.js";
 import { SECRET_KEY } from "../configs/env-config.js";
+import { nodeCache } from "./cache.js";
 
-const getSignature = (totalParams) => {
+export const getSignature = (totalParams) => {
   const queryString = querystring.stringify(totalParams);
   const signature = crypto
     .createHmac("sha256", SECRET_KEY)
@@ -14,46 +15,55 @@ const getSignature = (totalParams) => {
 
 // GET
 
-export const exchangeInformationAPI = async () => {
-  const response = await binanceFuturesAPI.get("/fapi/v1/exchangeInfo");
+export const getBinanceFuturesAPI = async (totalParams, path) => {
+  const signature = getSignature(totalParams);
+  const key = path + "/" + signature;
+  const cache = nodeCache.get(key);
+  if (cache) {
+    return cache;
+  }
+  const response = await binanceFuturesAPI.get(path, {
+    params: { ...totalParams, signature }
+  });
+  nodeCache.set(key, response.data);
   return response.data;
+};
+
+export const exchangeInformationAPI = async () => {
+  const responseData = await getBinanceFuturesAPI({}, "/fapi/v1/exchangeInfo");
+  return responseData;
 };
 
 export const futuresAccountBalanceAPI = async (totalParams) => {
-  const signature = getSignature(totalParams);
-  const response = await binanceFuturesAPI.get("/fapi/v2/balance", {
-    params: { ...totalParams, signature }
-  });
-  return response.data;
-};
-
-export const markPriceAPI = async (totalParams) => {
-  const response = await binanceFuturesAPI.get("/fapi/v1/premiumIndex", {
-    params: { ...totalParams }
-  });
-  return response.data;
+  const responseData = await getBinanceFuturesAPI(
+    totalParams,
+    "/fapi/v2/balance"
+  );
+  return responseData;
 };
 
 export const positionInformationAPI = async (totalParams) => {
-  const signature = getSignature(totalParams);
-  const response = await binanceFuturesAPI.get("/fapi/v2/positionRisk", {
-    params: { ...totalParams, signature }
-  });
-  return response.data;
+  const responseData = await getBinanceFuturesAPI(
+    totalParams,
+    "/fapi/v2/positionRisk"
+  );
+  return responseData;
 };
 
 export const markPriceKlineDataAPI = async (totalParams) => {
-  const response = await binanceFuturesAPI.get("/fapi/v1/markPriceKlines", {
-    params: { ...totalParams }
-  });
-  return response.data;
+  const responseData = await getBinanceFuturesAPI(
+    totalParams,
+    "/fapi/v1/markPriceKlines"
+  );
+  return responseData;
 };
 
 export const ticker24hrPriceChangeStatisticsAPI = async (totalParams) => {
-  const response = await binanceFuturesAPI.get("/fapi/v1/ticker/24hr", {
-    params: { ...totalParams }
-  });
-  return response.data;
+  const responseData = await getBinanceFuturesAPI(
+    totalParams,
+    "/fapi/v1/ticker/24hr"
+  );
+  return responseData;
 };
 
 // POST
