9687e367d28a7089a3d29aef470a7adfcbaf6fd8
Throuz <zed2786328@gmail.com>
2023-10-31 16:10:24 +0000

    change logic

--- configs/trade-config.js
+++ configs/trade-config.js
@@ -1,4 +1,6 @@
 export const QUOTE_ASSET = "USDT";
 export const LEVERAGE = 10;
+export const KLINE_INTERVAL = "1m";
+export const KLINE_LIMIT = 60;
 export const TRADING_RATIOS_PERIOD = "5m";
 export const ORDER_AMOUNT_PERCENT = 90;
--- src/conditions.js
+++ src/conditions.js
@@ -7,6 +7,13 @@ import {
   globalLongShortAccountRatioAPI
 } from "./api.js";
 import { nodeCache } from "./cache.js";
+import {
+  getMarkPrice,
+  getTrendAveragePrice,
+  getPositionInformation
+} from "./helpers.js";
+
+// Open conditions
 
 export const getIsLeverageAvailable = async () => {
   const symbol = nodeCache.get("symbol");
@@ -15,14 +22,6 @@ export const getIsLeverageAvailable = async () => {
     totalParams
   );
   return notionalAndLeverageBrackets[0].brackets[0].initialLeverage >= LEVERAGE;
-};
-
-export const getIsPriceAboveWeightedAvgPrice = async () => {
-  const symbol = nodeCache.get("symbol");
-  const totalParams = { symbol };
-  const statistics = await tickerPrice24hrChangeStatisticsAPI(totalParams);
-  const { lastPrice, weightedAvgPrice } = statistics;
-  return lastPrice > weightedAvgPrice;
 };
 
 export const getIsAllTradingRatiosBullish = async () => {
@@ -36,19 +35,81 @@ export const getIsAllTradingRatiosBullish = async () => {
   return results.every((result) => result[0].longShortRatio > 1);
 };
 
+export const getIsPriceInSafeZone = async () => {
+  const [markPrice, trendAveragePrice] = await Promise.all([
+    getMarkPrice(),
+    getTrendAveragePrice()
+  ]);
+  const isPriceInSafeZone = markPrice > trendAveragePrice;
+  return isPriceInSafeZone;
+};
+
+export const getIsPriceAboveWeightedAvgPrice = async () => {
+  const symbol = nodeCache.get("symbol");
+  const totalParams = { symbol };
+  const statistics = await tickerPrice24hrChangeStatisticsAPI(totalParams);
+  const { lastPrice, weightedAvgPrice } = statistics;
+  return lastPrice > weightedAvgPrice;
+};
+
 export const getIsOpenConditionsMet = async () => {
   const results = await Promise.all([
     getIsLeverageAvailable(),
-    getIsPriceAboveWeightedAvgPrice(),
-    getIsAllTradingRatiosBullish()
+    getIsAllTradingRatiosBullish(),
+    getIsPriceInSafeZone(),
+    getIsPriceAboveWeightedAvgPrice()
+  ]);
+  return results.every((result) => result);
+};
+
+// Close conditions
+
+export const getIsNotAllTradingRatiosBullish = async () => {
+  const isAllTradingRatiosBullish = await getIsAllTradingRatiosBullish();
+  return !isAllTradingRatiosBullish;
+};
+
+export const getIsUnRealizedProfitPositive = async () => {
+  const positionInformation = await getPositionInformation();
+  return positionInformation.unRealizedProfit > 0;
+};
+
+export const getIsUnRealizedProfitNegative = async () => {
+  const isUnRealizedProfitPositive = await getIsUnRealizedProfitPositive();
+  return !isUnRealizedProfitPositive;
+};
+
+export const getIsPriceNotInSafeZone = async () => {
+  const isPriceInSafeZone = await getIsPriceInSafeZone();
+  return !isPriceInSafeZone;
+};
+
+export const getIsPriceBelowWeightedAvgPrice = async () => {
+  const isPriceAboveWeightedAvgPrice = await getIsPriceAboveWeightedAvgPrice();
+  return !isPriceAboveWeightedAvgPrice;
+};
+
+export const getIsTakeProfit = async () => {
+  const results = await Promise.all([
+    getIsUnRealizedProfitPositive(),
+    getIsPriceNotInSafeZone()
+  ]);
+  return results.every((result) => result);
+};
+
+export const getIsStopLoss = async () => {
+  const results = await Promise.all([
+    getIsUnRealizedProfitNegative(),
+    getIsPriceBelowWeightedAvgPrice()
   ]);
   return results.every((result) => result);
 };
 
 export const getIsCloseConditionsMet = async () => {
   const results = await Promise.all([
-    getIsPriceAboveWeightedAvgPrice(),
-    getIsAllTradingRatiosBullish()
+    getIsNotAllTradingRatiosBullish(),
+    getIsTakeProfit(),
+    getIsStopLoss()
   ]);
-  return results.some((result) => result === false);
+  return results.some((result) => result);
 };
