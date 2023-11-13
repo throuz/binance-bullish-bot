469c9870064494346e0c920765b23d2a845d7ed4
Throuz <zed2786328@gmail.com>
2023-11-13 03:53:15 +0000

    change order amout percent

--- configs/trade-config.js
+++ configs/trade-config.js
@@ -1,5 +1,5 @@
 export const QUOTE_ASSET = "USDT";
 export const TRADING_RATIOS_PERIOD = "5m";
-export const ORDER_AMOUNT_PERCENT = 90;
+export const ORDER_AMOUNT_PERCENT = 61.8;
 export const TAKE_PROFIT_PERCENT = 61.8;
 export const STOP_LOSS_PERCENT = -38.2;
